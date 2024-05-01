9253fe955758c87b649c596fce3286921076035a
zubeir <zubwd1@gmail.com>
2024-05-01 16:59:35 +0100

    Ajout d'un nouveau champs de saisie pour les deux operation

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
 function calculate(operation) {
     let num1 = parseFloat(document.getElementById("num1").value);
     let num2 = parseFloat(document.getElementById("num2").value);
+    let num3 = parseFloat(document.getElementById("num2").value);
 
 
     var result;
 
     switch (operation) {
         case 'add':
-
-            result = num1 + num2;
+            if (!isNaN(num3)) {
+                result = num1 + num2 + num3;
+            } else {
+                result = num1 + num2;
+            }
 
             break;
         case 'subtract':
             result = num1 - num2;
             break;
         case 'multiply':
-
-            result = num1 * num2
+            if (!isNaN(num3)) {
+                result = num1 * num2 * num3;
+            } else {
+                result = num1 * num2;
+            }
 
         case 'divide':
             if (num2 === 0) {
